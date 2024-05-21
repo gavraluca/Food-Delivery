e90046a0993c0fc7cb7ba25253017eca47211457
Luca Gavra <66556065+LexusLuca@users.noreply.github.com>
2024-05-21 11:49:36 +0300

    Update Cart.cpp
    Added getTotalPrice function

--- Proiect FINAL PCLP 2/Cart.cpp
+++ Proiect FINAL PCLP 2/Cart.cpp
@@ -3,3 +3,10 @@
 void Cart::addProduct(Product* product) {
     products.push_back(product);
 }
+double Cart::getTotalPrice() const {
+    double total = 0;
+    for (Product* product : products) {
+        total += product->getPrice();
+    }
+    return total;
+}
