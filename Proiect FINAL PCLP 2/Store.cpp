2e82f164a4d3f6b7fd239b7e175d37b7311f0301
Luca Gavra <66556065+LexusLuca@users.noreply.github.com>
2024-05-21 12:06:27 +0300

    Update Store.cpp
    Added getName function and addProduct function

--- Proiect FINAL PCLP 2/Store.cpp
+++ Proiect FINAL PCLP 2/Store.cpp
@@ -2,3 +2,11 @@
 
 GroceryStore::GroceryStore(const std::string& name)
     : name(name) {}
+
+std::string GroceryStore::getName() const {
+    return name;
+}
+
+void GroceryStore::addProduct(Product* product) {
+    products.push_back(product);
+}
