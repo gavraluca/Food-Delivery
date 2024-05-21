38777ef64e1be8de7511a894835dfb30061e79b2
Luca Gavra <66556065+LexusLuca@users.noreply.github.com>
2024-05-21 11:56:40 +0300

    Update Product.cpp

--- Proiect FINAL PCLP 2/Product.cpp
+++ Proiect FINAL PCLP 2/Product.cpp
@@ -1,1 +1,8 @@
+#include "Product.h"
 
+Product::Product(const std::string& name, double price, int stock)
+    : name(name), price(price), stock(stock) {}
+
+std::string Product::getName() const {
+    return name;
+}
